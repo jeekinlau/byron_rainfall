ec5e094dc91390a73e341e797e0750d74999c134
jeekinlau <jeekinlau@gmail.com>
2026-01-23 16:18:33 -0500

    Auto-update rainfall data 2026-01-23 16:18:33 redundant

---
+++ README.md
@@ -0,0 +1,8 @@
+# byron_rainfall
+
+## ABOUT     
+This is a self updating page that will read the Byron weather station data provided by the UGA's Georgia Weather Network     
+ 
+The Forecast data comes from NOAA's API 
+ 
+[link to updating page](https://jeekinlau.github.io/byron_rainfall/)
---
+++ update_rainfall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Navigate to path where script is located
+cd ~/Desktop/rainfall_github 
+
+# Pull any updates
+git pull origin main
+
+# Run the R script
+Rscript weekly_rainfall.R
+
+# Add changes to git
+git add .
+
+
+# Commit changes with timestamp
+git commit -m "Auto-update rainfall data $(date '+%Y-%m-%d %H:%M:%S') redundant"
+
+
+
+# Push to GitHub
+git push origin main --force
